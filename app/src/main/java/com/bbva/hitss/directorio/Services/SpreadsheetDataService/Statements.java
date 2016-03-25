c18b9a2e0ee903fd8436bc779e291992b3025152
Castañeda <cristhian.castaneda@softianetworks.com>
2016-03-24 22:23:57 -0600

    :)

--- app/src/main/java/com/bbva/hitss/directorio/Services/SpreadsheetDataService/Statements.java
+++ app/src/main/java/com/bbva/hitss/directorio/Services/SpreadsheetDataService/Statements.java
@@ -7,4 +7,5 @@ public class Statements {
     //public static String selectAll = "https://spreadsheets.google.com/tq?tqx=out:JSON&tq=select%20*%20order%20by%20C&key=1yN29RRdxwtp6li6vBAChH8iruIR_C3EqO50KPAi_oJg&gid=2100890770";
     public static String selectAll = "https://docs.google.com/spreadsheets/d/1yN29RRdxwtp6li6vBAChH8iruIR_C3EqO50KPAi_oJg/gviz/tq?tqx=out:JSON&tq=select+*+order+by+C";
     public static String internosBBVA = "https://docs.google.com/spreadsheets/d/1yN29RRdxwtp6li6vBAChH8iruIR_C3EqO50KPAi_oJg/gviz/tq?tqx=out:JSON&tq=select+*&gid=2100890770";
+
 }
